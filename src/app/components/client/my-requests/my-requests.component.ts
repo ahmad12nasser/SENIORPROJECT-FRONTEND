8ac0e90c8456eecee20e77cd39d29f2112eea84b
ahmad12nasser <ahmad749nasser123@gmail.com>
2024-03-11 23:43:49 +0200

    fetching requests by status 'Pending' and building the control room for both users and editing the necessary files

--- src/app/components/client/my-requests/my-requests.component.ts
+++ src/app/components/client/my-requests/my-requests.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { request } from '../../../models/request';
+import { GetMyRequestsService } from './../../../services/client/getMyRequests.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-my-requests',
   templateUrl: './my-requests.component.html',
   styleUrl: './my-requests.component.css'
 })
-export class MyRequestsComponent {
+export class MyRequestsComponent implements  OnInit{
 
+  requests: request[] = [];
+
+  constructor(private getMyRequestsService: GetMyRequestsService) { }
+
+  ngOnInit() {
+    //const clientId = this.getFreelancerIdFromLocalStorage();
+    const clientId = 1;
+    // Fetch requests when the component initializes
+    this.getMyRequestsService.getMyRequests(clientId).subscribe({
+      next: (data) => {
+        this.requests = data;
+               console.log('Successfully fetched requests:', true);
+      },
+      error: (error) => {
+        console.error('Error fetching requests:', error);
+      }
+      
+    });
+  }
+  //this method ta 7atta es7ab el clientId mn el local session
+  private getClientIdFromLocalSession(): number {
+    return Number(localStorage.getItem('userId'));
+  }
 }
